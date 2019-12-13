e20f56c56c44ed318c73fb50eeb11012c5164f20
Shubham Pandey <shubhampandey1097@gmail.com>
2019-12-13 12:18:29 +0530

    updated again

--- src/components/Button.js
+++ src/components/Button.js
@@ -9,7 +9,7 @@ export const ButtonContainer = styled.button`
   color: var(--lightBlue) ;
   border-radius: 0.5rem ;
   padding:0.2rem 0.5rem ;
-  cursor: pointer;
+  cursor: pointer ;
   margin: 0.2rem 0.5rem 0.2rem 0 ;
   transition: all 0.5s ease-in-out ;
 {/*shubham: & -->>> applies on ButtonContainer only while hovering*/}
--- src/data.js
+++ src/data.js
@@ -18,6 +18,19 @@ export const storeProducts = [
     total: 0
   },
   {
+    id: 1,
+    title: "Google Pixel - Black",
+    //Shubham : {/*Looking for images in public folder*/}
+    img: "img/product-1.png",
+    price: 10,
+    company: "GOOGLE",
+    info:
+      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
     id: 2,
     title: "Samsung S7",
     img: "img/product-2.png",
